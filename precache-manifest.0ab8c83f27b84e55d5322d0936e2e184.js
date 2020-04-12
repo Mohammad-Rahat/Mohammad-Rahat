6d1a4a4b19af6f34577efe53a6684986a733b65c
Mohammad-Rahat <errahatinfinity@gmail.com>
2020-04-12 11:54:53 +0600

    Updates

---
+++ precache-manifest.0ab8c83f27b84e55d5322d0936e2e184.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3583b26b3946dd88d64d38879b45f6b9",
+    "url": "/ralfolio/index.html"
+  },
+  {
+    "revision": "13bfe1760d787c458c3b",
+    "url": "/ralfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "42adf3cdf6e75a8eb8b5",
+    "url": "/ralfolio/static/css/main.eedfbdc1.chunk.css"
+  },
+  {
+    "revision": "13bfe1760d787c458c3b",
+    "url": "/ralfolio/static/js/2.8500c394.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/ralfolio/static/js/2.8500c394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42adf3cdf6e75a8eb8b5",
+    "url": "/ralfolio/static/js/main.5c8f2c6f.chunk.js"
+  },
+  {
+    "revision": "42f22c176525e6d9a475",
+    "url": "/ralfolio/static/js/runtime-main.41810f8b.js"
+  },
+  {
+    "revision": "8b348676e97acb5c750dbdaab67d4747",
+    "url": "/ralfolio/static/media/Comfortaa.8b348676.ttf"
+  },
+  {
+    "revision": "36e7a8bfb3f250ddc37493130add89e5",
+    "url": "/ralfolio/static/media/fa-brands-400.36e7a8bf.woff"
+  },
+  {
+    "revision": "4a60307193b4dcf411a958f449b2f599",
+    "url": "/ralfolio/static/media/fa-brands-400.4a603071.svg"
+  },
+  {
+    "revision": "764a8efba3c8a098aa85b4c37b8155a6",
+    "url": "/ralfolio/static/media/fa-brands-400.764a8efb.eot"
+  },
+  {
+    "revision": "8dbeb0ea6cd5f1f5cf6ec094dfbfb96c",
+    "url": "/ralfolio/static/media/fa-brands-400.8dbeb0ea.ttf"
+  },
+  {
+    "revision": "c4af52f53368b81cc3ea577f37f9a916",
+    "url": "/ralfolio/static/media/fa-brands-400.c4af52f5.woff2"
+  },
+  {
+    "revision": "09830468cd0d8b16d6e213b1c888744f",
+    "url": "/ralfolio/static/media/fa-duotone-900.09830468.svg"
+  },
+  {
+    "revision": "2a6dec1227f9970376f578270a642d06",
+    "url": "/ralfolio/static/media/fa-duotone-900.2a6dec12.woff2"
+  },
+  {
+    "revision": "e084f1c8b648d7d45a50b0dae900c8b6",
+    "url": "/ralfolio/static/media/fa-duotone-900.e084f1c8.ttf"
+  },
+  {
+    "revision": "e6a9123b6d6d89911e7a3d7f01e33f82",
+    "url": "/ralfolio/static/media/fa-duotone-900.e6a9123b.eot"
+  },
+  {
+    "revision": "ea3d65a7b9829c9090b0a80c6e260a4a",
+    "url": "/ralfolio/static/media/fa-duotone-900.ea3d65a7.woff"
+  },
+  {
+    "revision": "0037347f16a6dd10447b0b2911b0104c",
+    "url": "/ralfolio/static/media/fa-light-300.0037347f.ttf"
+  },
+  {
+    "revision": "86683af0360cc024c998f00de4c43e9e",
+    "url": "/ralfolio/static/media/fa-light-300.86683af0.woff2"
+  },
+  {
+    "revision": "97468fa17b539584bbe0b351319d66e7",
+    "url": "/ralfolio/static/media/fa-light-300.97468fa1.eot"
+  },
+  {
+    "revision": "d8e14023b11ab348bcd6148a50b7ae85",
+    "url": "/ralfolio/static/media/fa-light-300.d8e14023.woff"
+  },
+  {
+    "revision": "ef7b6ed77b842d9524ffb0f297fcc6d9",
+    "url": "/ralfolio/static/media/fa-light-300.ef7b6ed7.svg"
+  },
+  {
+    "revision": "39bd303fbf945408f7f6e585bc086ee4",
+    "url": "/ralfolio/static/media/fa-regular-400.39bd303f.ttf"
+  },
+  {
+    "revision": "75721d809c7b38cce914a6bd07b0973b",
+    "url": "/ralfolio/static/media/fa-regular-400.75721d80.eot"
+  },
+  {
+    "revision": "983a20d28fbf8fbc27cfeb096f0ebd85",
+    "url": "/ralfolio/static/media/fa-regular-400.983a20d2.woff2"
+  },
+  {
+    "revision": "dd012cb958a95fc77ced4ecbb7c87040",
+    "url": "/ralfolio/static/media/fa-regular-400.dd012cb9.woff"
+  },
+  {
+    "revision": "eab6a454886d3c4f7968c4be69ca5c55",
+    "url": "/ralfolio/static/media/fa-regular-400.eab6a454.svg"
+  },
+  {
+    "revision": "0259646b38bc6f3409fe2567a94c37b8",
+    "url": "/ralfolio/static/media/fa-solid-900.0259646b.svg"
+  },
+  {
+    "revision": "265b724844b97d7427fc4a44c9c2e43d",
+    "url": "/ralfolio/static/media/fa-solid-900.265b7248.woff"
+  },
+  {
+    "revision": "27df36c7e2bec8e3624bed94f02e7aa8",
+    "url": "/ralfolio/static/media/fa-solid-900.27df36c7.ttf"
+  },
+  {
+    "revision": "9b39dfc9bafe5944979aff454bdfe44f",
+    "url": "/ralfolio/static/media/fa-solid-900.9b39dfc9.eot"
+  },
+  {
+    "revision": "e245d7d62db89a9912e17b02bc23e256",
+    "url": "/ralfolio/static/media/fa-solid-900.e245d7d6.woff2"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "/ralfolio/static/media/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "/ralfolio/static/media/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "/ralfolio/static/media/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "/ralfolio/static/media/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "4515c2978bed8d508d7d13181ff6b296",
+    "url": "/ralfolio/static/media/natour.4515c297.jpg"
+  },
+  {
+    "revision": "e0f54ad71a7d76a738cf6ff279978b86",
+    "url": "/ralfolio/static/media/nexter.e0f54ad7.jpg"
+  },
+  {
+    "revision": "1c34a561ada0912dc1bae9b4608a3089",
+    "url": "/ralfolio/static/media/pokedex.1c34a561.jpg"
+  },
+  {
+    "revision": "0113092dd99fcef355949d398f7239dd",
+    "url": "/ralfolio/static/media/ptotographi.0113092d.jpg"
+  },
+  {
+    "revision": "ed08278d7f04507080a9bc36b960b077",
+    "url": "/ralfolio/static/media/resume-photo.ed08278d.jpg"
+  },
+  {
+    "revision": "e49f092ab9fe5f83085e5eb8cc8b5473",
+    "url": "/ralfolio/static/media/smartbrain.e49f092a.jpg"
+  },
+  {
+    "revision": "13875702033e4172de6df326079bbfd1",
+    "url": "/ralfolio/static/media/trillo.13875702.jpg"
+  }
+]);
